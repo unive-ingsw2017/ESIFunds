bc50be642b2917afdb71c9c5c5f4dd27d8173d0b
Nildric <longhin.federico.nildric@gmail.com>
2018-01-28 19:27:17 +0100

    Map Update

--- app/src/main/java/com/esifunds/fragment/FragmentMap.java
+++ app/src/main/java/com/esifunds/fragment/FragmentMap.java
@@ -15,6 +15,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FragmentMap extends Fragment implements OnMapReadyCallback
 {
@@ -43,7 +54,43 @@ public class FragmentMap extends Fragment implements OnMapReadyCallback
 
         fragmentGoogleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.map_style));
 
+        // HeatMap Code
+        List<LatLng> latLngList = null;
+        try
+        {
+            latLngList = buildLatLngList(R.raw.map_heat);
+        }
+        catch(JSONException jsonException)
+        {
+            jsonException.printStackTrace();
+        }
+
+        HeatmapTileProvider heatmapTileProvider = new HeatmapTileProvider.Builder().data(latLngList).radius(50).build();
+        TileOverlayOptions tileOverlayOptions = new TileOverlayOptions().tileProvider(heatmapTileProvider);
+        fragmentGoogleMap.addTileOverlay(tileOverlayOptions);
+
         fragmentGoogleMap.setLatLngBoundsForCameraTarget(latLngBoundsItaly);
         fragmentGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(latLngBoundsItaly, 0));
+    }
+
+    private ArrayList<LatLng> buildLatLngList(int resIdentifier) throws JSONException
+    {
+        ArrayList<LatLng> latLngArrayList = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resIdentifier);
+
+        String jsonString = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray jsonArray = new JSONArray(jsonString);
+
+        for(int i = 0; i < jsonArray.length(); i++)
+        {
+            JSONObject jsonObject =  jsonArray.getJSONObject(i);
+
+            double lat = jsonObject.getDouble("lat");
+            double lng = jsonObject.getDouble("lng");
+
+            latLngArrayList.add(new LatLng(lat, lng));
+        }
+
+        return latLngArrayList;
     }
 }
